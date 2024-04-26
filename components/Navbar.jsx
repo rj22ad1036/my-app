81237ca3182bba2fb9a1e1020603e5fac7160729
rj22ad1036 <rohanjoshijoshi50@gmail.com>
2024-04-26 14:18:25 +0530

    nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import BookIcon from "./icons/Bookicon";
+import MaskIcon from "./icons/Mask";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row w-full h-26 p-4 bg-blue-500">
-      <div className="w-1/2 text-3xl font-semibold text-indigo-800 flex flex-row gap-2">
+    <div className="flex flex-row w-full h-26 p-4 bg-blue-500 pl-5 pr-5">
+      <div className="w-1/2 text-3xl font-semibold text-indigo-800 flex flex-row gap-3">
         <div>
           <BookIcon />
         </div>
         <div> ViChron</div>
       </div>
-      <div className="flex justify-between w-1/2  text-xl text-white">
+      <div className="flex justify-between w-1/2  text-xl text-white font-semibold ">
         <a href="/home">Home</a>
         <a href="/about">About</a>
         <a href="/login">Login</a>
